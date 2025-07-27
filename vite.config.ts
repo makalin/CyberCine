ec3572906f3164b40922a79767451c14c4cacff7
Mehmet T. AKALIN <makalin@users.noreply.github.com>
2025-07-27 18:28:29 +0300

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/CyberCine/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`
+      }
+    }
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
